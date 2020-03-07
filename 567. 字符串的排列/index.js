c57339bbaae839f982076755b8d575f8344f5cb5
ZhangYxY <512224856@qq.com>
2020-03-07 19:52:05 +0800

    feat: 字符串的排列

--- 567. 字符串的排列/index.js
+++ 567. 字符串的排列/index.js
@@ -4,11 +4,26 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    let a = s1.split('');
-    
-    return s2.indexOf(s1) > -1;
+    let n1 = s1.length,n2 = s2.length;
+    if(n1 > n2) return false;
+    let count = new Array(26).fill(0);
+    let isAllZero = (count) => {
+        return count.every( val => val === 0);
+    }
+    for(let i = 0;i < n1;i++){
+        count[s1[i].charCodeAt() - 97]++;
+        count[s2[i].charCodeAt() - 97]--;
+    }
+    if(isAllZero(count)) return true;
+    for(let i = n1;i < n2;i++){
+        count[s2[i].charCodeAt() - 97]--;
+        count[s2[i - n1].charCodeAt() - 97]++;
+        if(isAllZero(count)) return true;
+    }
+    return false;
 };
 
-let s1 = "ab", s2 = "eidbaooo";
+// let s1 = "ab", s2 = "eidbaooo";
+let s1= "abaaaaaaaaaa", s2 = "eidabaaaaaaaaaaboaoo";
 let r = checkInclusion(s1, s2);
 console.log(r);
